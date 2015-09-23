0f7c9f343be412b412564af9f0a369086d4e71a1
Daniel <daniel@forter.com>
2015-09-23 21:14:39 +0300

    Added some comments

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,7 +5,7 @@ void Parser::run(){// need to edit this
     
     for(int i = 0;i < toks.size();i++ ){
         auto currentTokenType = toks[i].type;
-        if(currentTokenType == tokType::FUNCTION){
+        if(currentTokenType == tokType::FUNCTION){// The function token type only appears in function callings we didn't make function defenitions yet
             if(prevParent != nullptr){
                 createNode(prevParent, NodeType::FUNCTION, currentStatement); // need to add checking for this. eg instead of Function() we have () without a string before
             }
@@ -13,7 +13,7 @@ void Parser::run(){// need to edit this
         }
         else if(currentTokenType != tokType::DELIMITER || currentTokenType != tokType::CLOSINGPARAN){
             currentStatement.push_back(toks[i]);
-            prevStatement = currentStatement;
+            prevStatement = currentStatement;// Shouldn't this be in the else section?
             
         }
         else{
