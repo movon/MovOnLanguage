2fe93c519b0c89b68ce4cefd7bfd43fb482cb844
RoyChuChun <roy.shulman@gmail.com>
2016-09-03 16:34:33 +0300

    Finished statement class

--- src/Grammar/Program/FunctionGrammar/Statement.cpp
+++ src/Grammar/Program/FunctionGrammar/Statement.cpp
@@ -3,10 +3,52 @@
 Node* Statement::tryParse(TokStreamer* st){
 	int save = st->getIndex();
 	Node* mainNode = nullptr;
-
-
-
-
+	Node* nextNode = nuTokStreamer* stllptr;
+	//check for a functiondef
+	mainNode = functionDef::tryParse(st);
+	if(mainNode != nullptr){
+		//goto the next statement
+		nextNode = Statement::tryParse(st);
+		mainNode->addChild(nextNode);
+		return mainNode;
+	}
+	st->setIndex(save);
+	//check for a function call
+	mainNode = FuncCall::tryParse(st);
+	else if(mainNode != nullptr){
+		///goto the next statement
+		nextNode = Statement::tryParse(st);
+		mainNode->addChild(nextNode);
+		return mainNode;
+	}
+	st->setIndex(save);
+	//check for a flow statement
+	mainNode = Flow::tryParse(st);
+	else if(mainNode != nullptr){
+		//goto the next statement
+		nextNode = Statement::tryParse(st);
+		mainNode->addChild(nextNode);
+		return mainNode;
+	}
+	st->setIndex(save);
+	//check for an assignment
+	mainNode = Assignment::tryParse(st);
+	else if(mainNode != nullptr){
+		//goto the next statement
+		nextNode = Statement::tryParse(st);
+		mainNode->addChild(nextNode);
+		return mainNode;
+	}
+	st->setIndex(save);
+	//check for a keyword
+	mainNode = Keyword::tryParse(st);
+	else if(mainNode != nullptr){
+		//goto the next statement
+		nextNode = Statement::tryParse(st);
+		mainNode->addChild(nextNode);
+		return mainNode
+	}
+	//it's not a statement return nullptr
 	return mainNode;
 
 }
